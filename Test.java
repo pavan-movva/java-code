e261733b83dda0b2d15a4154a4e4015be22a5d4c
pavan-movva <pavan.movva@aliet.ac.in>
2024-09-10 15:21:37 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test
   {
     public static void main(String[] args)
     {
-      System.out.println("java github jenkins");
+      System.out.println("welcome to java github jenkins");
     }
   }
