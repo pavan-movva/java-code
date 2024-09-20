bc257cf2b083d8a0f60312e8c2b4785337cc5534
pavan-movva <pavan.movva@aliet.ac.in>
2024-09-20 12:29:43 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test
   {
     public static void main(String[] args)
     {
-      System.out.println("git polling from jenkins");
+      System.out.println("20th september");
     }
   }
