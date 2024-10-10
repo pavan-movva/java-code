6d41d367c705a1167f49f860e4064d6d5df15f95
pavan-movva <pavan.movva@aliet.ac.in>
2024-10-10 14:15:23 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,12 @@ public class Test
   {
     public static void main(String[] args)
     {
+      int a=0,b=0,c;
+      c=1/b;
+      int d;
+      while(TRUE)
+        {
+        }
       System.out.println("bhanu prakash cr changes");
     }
   }
